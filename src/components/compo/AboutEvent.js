5306933aad1d253b0fa7f93e7e35f9a8efdde5b7
mouad ouakil <mouadeouakil@gmail.com>
2022-01-04 22:40:16 +0100

    about & footer

---
+++ src/components/compo/AboutEvent.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import './AboutEvent.css';
+function aboutEvent() {
+    return (
+        <div className='aboutEvent'>
+            <h1>About this event</h1>
+            <p> 
+            During a day of sessions and workshops in the capital, the growing technological city of Costa del Sol, world-class experts in multiple areas and disciplines meet.
+
+With the attendance of more than 90% of technical profiles BiznagaFest is one of the largest events focused on technology in Andalusia.
+Young talents and industry veterans come together for a journey to share knowledge and build professional relationships.
+
+An event about technology and business that pampers the details and in which the quality of the sessions or workshops is as important as the creation of connections between the attendees.
+
+
+
+            </p>
+            <p><h3>Join us!
+            As an attendee
+                You must RSVP in this event, and get a ticket in our Eventbrite page.</h3></p>
+
+
+        </div>
+    )
+}
+
+export default aboutEvent
--- src/components/pages/Event.js
+++ src/components/pages/Event.js
@@ -2,12 +2,17 @@ import React from 'react';
 import EventComp from'../compo/eventComp.js';
 import RSVP from '../compo/RSVP.js';
 import Speakers from '../compo/speakers.js';
+import AboutEvent from '../compo/AboutEvent.js';
+import Footer from '../compo/Footer.js';
+
 export default function Event() {
   return (
     <>
       <EventComp/>
       <RSVP/>
       <Speakers/>
+      <AboutEvent/>
+      <Footer/>
     </>
   );
 }
